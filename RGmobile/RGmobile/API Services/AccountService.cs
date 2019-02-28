8b0b6320a6f5056a5a1066081c4b6497ba697a15
Filipe Catarcione <filip@DESKTOP-7NPPLQS.gsgus.com>
2019-02-27 23:26:42 -0500

    Add login view and logic

---
+++ RGmobile/RGmobile/API Services/AccountService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGmobile.API_Services
+{
+    public class AccountService
+    {
+        //Get Request
+        public async Task<string> Login()
+        {
+            var client = new HttpClient();
+
+            //send request to server. Server will send response back in JSON format
+            var response = await client.GetStringAsync("https://localhost:44308/api/User/login");
+            return JsonConvert.DeserializeObject<string>(response);
+        }
+
+        //Post Request
+        //public async Task<bool> ReserveTable(Reservation reservation)
+        //{
+        //    var client = new HttpClient();
+
+        //    //send data to server in JSON format
+        //    string json = JsonConvert.SerializeObject(reservation);
+
+        //    var content = new StringContent(json, Encoding.UTF8, "application/json");
+        //    var response = await client.PostAsync("https://expresso-api.azurewebsites.net/api/Reservations", content);
+
+        //    return response.IsSuccessStatusCode;
+        //}
+
+
+    }
+}
---
+++ RGmobile/RGmobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,66 @@
+﻿using RGmobile.API_Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RGmobile.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private AccountService _accountService;
+
+        private string _username;
+        public string UserName
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoginCommand { get; private set; }
+
+        #region Constructor
+
+        public LoginViewModel()
+        {
+            _accountService = new AccountService();
+            LoginCommand = new Command(Login);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void Login()
+        {
+            var token = await _accountService.Login();
+        }
+
+        #endregion
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
